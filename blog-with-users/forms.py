6fc3fa80c6f91d9e79813ab932c4ddf471fe3b3e
EZYCJ <94887421+EZYCJ@users.noreply.github.com>
2023-09-29 18:19:23 +0530

    Add files via upload

--- blog-with-users/forms.py
+++ blog-with-users/forms.py
@@ -1,16 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
-from flask_ckeditor import CKEditorField
+# from flask_ckeditor import CKEditorField
 
-
-# WTForm for creating a blog post
-class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
 
 
 # Create a form to register new users
@@ -29,6 +21,6 @@ class LoginForm(FlaskForm):
 
 
 # Create a form to add comments
-class CommentForm(FlaskForm):
-    comment_text = CKEditorField("Comment", validators=[DataRequired()])
-    submit = SubmitField("Submit Comment")
+# class CommentForm(FlaskForm):
+#     comment_text = CKEditorField("Comment", validators=[DataRequired()])
+#     submit = SubmitField("Submit Comment")
